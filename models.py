87fe0936e607cde5c1374435c1ab22a2e09db0b7
Irfan <irfan93940@gmail.com>
2025-03-21 16:55:59 +0530

    added correct server_default=text('now()')

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 
 class Post(Base):
     __tablename__ = "posts"
@@ -8,7 +10,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     # published = Column(Boolean, default=True)  This is not the correct method to set default
     published = Column(Boolean, server_default='TRUE', nullable=False)
+    # created_at = Column(TIMESTAMP(timezone=True), server_default='now()')
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
 
-
-    
 
