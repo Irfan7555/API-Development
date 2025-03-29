a0ff9567ca3f0808cc820a5492a1b87df8b3cc09
irfan7555 <irfan93940@gmail.com>
2025-03-29 14:40:12 +0530

    postman usage instruction

--- routers/auth.py
+++ routers/auth.py
@@ -21,7 +21,7 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session= 
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials")
 
     access_token = create_access_token(data= {"user_id":user.id})
-
+    # Use form data field in postman to test this functionality
 
     return {"access_token": access_token, "token_type": "bearer"}
 
